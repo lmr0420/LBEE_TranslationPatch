3ea8a4031df7701cf23695e3f5892cf4e99c4d01
JackMyth <wwwbkkk@126.com>
2025-01-18 22:58:29 +0800

    修正文本中的符号

---
+++ CheckMessage.ps1
@@ -0,0 +1,27 @@
+
+Set-Location $PSScriptRoot
+
+$JsonFiles = Get-ChildItem ./TextMapping
+foreach($JsonFile in $JsonFiles)
+{
+    $Source = ConvertFrom-Json -InputObject (Get-Content $JsonFile.FullName -Raw)
+    if($null -ne $Source.MESSAGE)
+    {
+        foreach($MessageItem in $Source.MESSAGE)
+        {
+            if($MessageItem -eq $null)
+            {
+                break;
+            }
+            if($MessageItem.JP.Contains("@") -ne $MessageItem.Translation.Contains("@"))
+            {
+                Write-Output "Error in $($JsonFile.Name): $($MessageItem.JP) -> $($MessageItem.Translation)"
+            }
+            elseif ($MessageItem.JP.StartsWith("``") -ne $MessageItem.Translation.StartsWith("``"))
+            {
+                Write-Output "Error in $($JsonFile.Name): $($MessageItem.JP) -> $($MessageItem.Translation)"
+            }
+        }
+    }
+    
+}
